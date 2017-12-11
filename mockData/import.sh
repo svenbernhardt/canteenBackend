607ce35b985958c67737203210a881924bf53117
Bernhardt, Sven <Sven.Bernhardt@opitz-consulting.com>
2017-12-11 22:09:05 +0100

    Added wrecker.yml; Change import host in import.sh

--- mockData/import.sh
+++ mockData/import.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mongoimport --host 127.0.0.1 --db steak --collection orders --type json orders.json
-mongoimport --host 127.0.0.1 --db steak --collection offers --type json offers.json
+mongoimport --host 129.144.149.167 --db steak --collection orders --type json orders.json
+mongoimport --host 129.144.149.167 --db steak --collection offers --type json offers.json
